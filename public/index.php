7e48ecf212500d81f796f9c1574b7702b92d6ae0
Alen Kalac <alenkalac87@gmail.com>
2016-04-18 20:11:39 +0100

    switching to atom

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../app/setup.php';
 
 $app->get ( '/', 'alen\MainController::indexPage' );
 $app->get ( '/test/{name}', 'alen\MainController::testPage' );
+$app->get ('/test2', 'alen\MainController::macroTest');
 
 $app->run ();
 ?>
--- src/MainController.php
+++ src/MainController.php
@@ -7,21 +7,25 @@ use Symfony\Component\HttpFoundation\Request;
 
 class MainController {
 	public function indexPage(Request $request, Application $app) {
-		$args = [ 
+		$args = [
 				'name' => 'Alen2',
-				'title' => 'test' 
+				'title' => 'test'
 		];
-		
+
 		return $app ['twig']->render ( 'index.html.twig', $args );
 	}
-	
+
 	public function testPage($name, Request $request, Application $app) {
 		$args = [
 				'name' => $name,
 				'title' => 'test'
 		];
-	
+
 		return $app ['twig']->render ( 'index.html.twig', $args );
+	}
+
+	public function macroTest(Request $request, Application $app) {
+		return $app['twig']->render('test.html.twig');
 	}
 }
 
